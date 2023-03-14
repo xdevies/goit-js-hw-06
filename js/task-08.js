64cab5c742cf2a15638d64a6cb3f356ec4b8cc05
xdevies <117025533+xdevies@users.noreply.github.com>
2023-03-15 00:56:22 +0100

    add task 8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (!email || !password) {
+    alert('Все поля должны быть заполнены!');
+    return;
+  }
+
+  const userData = { email, password };
+
+  console.log(userData);
+
+  form.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const colorSpan = document.querySelector('.color');
+button.addEventListener('click', function() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
